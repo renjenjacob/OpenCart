53c650cef15624f471fd83ed7c61a0909a67f7a2
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2024-10-06 12:42:29 -0400

    Updated Hello class with the implementation

--- src/test/java/roughWork/Hello.java
+++ src/test/java/roughWork/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("our module is ending in 2 days");
-
+		//This class is developed by one of my favorite students
+		//I am optimistic about his/her success.
 	}
 
 }
